d2b6273a1e81265972f0b7412a57c620d5d1ac72
Yenny <107637741+yenny42@users.noreply.github.com>
2023-11-24 20:38:20 +0900

    iOS 앱개발 강의 2주차

---
+++ MySecondProject/MySecondProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySecondProject
+//
+//  Created by t2023-m0035 on 11/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
