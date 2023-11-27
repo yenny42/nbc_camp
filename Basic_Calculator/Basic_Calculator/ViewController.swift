01ca79c6c43b977eaf208ff1f58a5c5a8af4d1e7
Yenny <107637741+yenny42@users.noreply.github.com>
2023-11-27 15:44:49 +0900

    basic calculator project

---
+++ Basic_Calculator/Basic_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic_Calculator
+//
+//  Created by t2023-m0035 on 11/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
