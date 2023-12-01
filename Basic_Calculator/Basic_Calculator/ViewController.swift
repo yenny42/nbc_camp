08fab946269ae11b36ac66256ae19bfca46ffdff
yenny42 <yennydev42@gmail.com>
2023-12-01 16:39:50 +0900

    - Calculator 클래스 파일 분리 - import Foundation 삭제 - 사용하지 않는 IBOutlet 삭제 - 코드 개행 통일 - checkDouble 함수 수정

--- Basic_Calculator/Basic_Calculator/ViewController.swift
+++ Basic_Calculator/Basic_Calculator/ViewController.swift
@@ -1,21 +1,17 @@
 //
 //  ViewController.swift
+//  계산기 UI
 //  Basic_Calculator
 //
 //  Created by t2023-m0035 on 11/27/23.
 //
 
 import UIKit
-import Foundation
 
 class ViewController: UIViewController {
     
 //    MARK: - IBOutlet
-    @IBOutlet weak var clearButton: UIButton!
-    @IBOutlet weak var numButton: UIButton!
-    @IBOutlet weak var calculateButton: UIButton!
     @IBOutlet weak var displayLable: UILabel!
-    @IBOutlet weak var equalSignButton: UIButton!
     
     //    MARK: - 변수
     var curreuntDisplay = "" // 내가 지금 입력한 값을 보여주기 위한 문자열
@@ -29,22 +25,22 @@ class ViewController: UIViewController {
     var calculatorMul = Calculator(abstractOperation: MultiplyOperation())
     var calculatorDiv = Calculator(abstractOperation: DivideOperation())
     
-    
 //    MARK: - view load
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
 //    MARK: - function
-    func checkDouble(value: Double) -> Bool {
-        return value.truncatingRemainder(dividingBy: 1.0) == 0 ? true : false
+    func checkDouble(value: Double) -> String {
+        let check = value.truncatingRemainder(dividingBy: 1.0) == 0 ? true : false
+        
+        return check ? String(Int(value)) : String(value)
     }
     
     func basicOperations(_ type: String) {
-        
         if numberValue[0] != 0 && numberValue[1] != 0 {
-            
             var result: Double = 0
+                
             switch type {
                 case "+":
                     result = calculatorAdd.calculate(numberValue, type)
@@ -61,17 +57,14 @@ class ViewController: UIViewController {
                 default:
                     break
             }
-            
+        
             numberValue = [result, 0]
-            displayLable.text = checkDouble(value: numberValue[0]) ? String(Int(numberValue[0])) : String(numberValue[0])
-            
+            displayLable.text = checkDouble(value: numberValue[0])
             curreuntDisplay = ""
-            
         }
     }
     
 //    MARK: - IBAction
-    
     @IBAction func didTapNumButton(_ sender: UIButton) {
         curreuntDisplay += sender.currentTitle!
         displayLable.text = curreuntDisplay
@@ -83,11 +76,9 @@ class ViewController: UIViewController {
         if numberValue[0] == 0 && curreuntDisplay != "" {
             numberValue[0] = numberValue[1]
             numberValue[1] = Double(curreuntDisplay)!
-                
             basicOperations(operatorSign)
         } else {
             numberValue[0] == 0 ? (numberValue[0] = Double(curreuntDisplay)!) : (numberValue[1] = (curreuntDisplay != "" ? Double(curreuntDisplay)! : 0))
-                
             basicOperations(operatorSign)
         }
             
@@ -104,7 +95,6 @@ class ViewController: UIViewController {
     }
     
     @IBAction func didTapEqualSignButton(_ sender: UIButton) {
-        
         if numberValue[1] == 0 && curreuntDisplay != "" {
             numberValue[1] = Double(curreuntDisplay)!
         } else {
@@ -114,55 +104,5 @@ class ViewController: UIViewController {
         
         basicOperations(equalOpeSign)
     }
-    
 }
 
-
-// MARK: - Class Calculator
-class Calculator {
-    
-    private var abstractOperation: AbstractOperation
-    
-    init(abstractOperation: AbstractOperation) {
-        self.abstractOperation = abstractOperation
-    }
-    
-    func setOperation(operation: AbstractOperation) {
-        self.abstractOperation = operation
-    }
-    func calculate(_ numbers: [Double], _ operatorType: String) -> Double {
-        abstractOperation.operate(numbers)
-    }
-}
-// MARK: - AbstractOperation Override
-class AbstractOperation {
-    func operate(_ numbers:[Double]) -> Double {
-        return 0
-    }
-}
-class AddOperation: AbstractOperation {
-    override func operate(_ numbers: [Double]) -> Double {
-        return numbers[0] + numbers[1]
-    }
-}
-class SubtractOperation: AbstractOperation {
-    override func operate(_ numbers: [Double]) -> Double {
-        return numbers[0] - numbers[1]
-    }
-}
-class MultiplyOperation: AbstractOperation {
-    override func operate(_ numbers:[Double]) -> Double {
-        return numbers[0] * numbers[1]
-    }
-}
-class DivideOperation: AbstractOperation {
-    override func operate(_ numbers:[Double]) -> Double {
-        return numbers[0] / numbers[1]
-    }
-}
-//class RemainderOperation: AbstractOperation {
-//    override func operate(_ numbers: [Double]) -> Double {
-//        let value = numbers[0].truncatingRemainder(dividingBy: numbers[1])
-//        return value
-//    }
-//}
