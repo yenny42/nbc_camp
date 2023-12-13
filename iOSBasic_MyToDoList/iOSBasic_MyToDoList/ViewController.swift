34ada003e0100f86d6afe0d7afb492f3151e4afd
yenny42 <yennydev42@gmail.com>
2023-12-13 09:28:13 +0900

    create MyTodoList project

---
+++ iOSBasic_MyToDoList/iOSBasic_MyToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSBasic_MyToDoList
+//
+//  Created by t2023-m0035 on 12/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
