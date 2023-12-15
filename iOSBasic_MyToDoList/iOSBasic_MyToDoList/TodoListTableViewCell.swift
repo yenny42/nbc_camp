a3a834ed649a56464094f717cc433867fe6937a6
yenny42 <yennydev42@gmail.com>
2023-12-15 11:41:01 +0900

    ✨ [Lv2] 완료,추가 / [Lv3] 삭제 - UIAlertController - deleteRows(at:with:) - 프로퍼티 옵저버

--- iOSBasic_MyToDoList/iOSBasic_MyToDoList/TodoListTableViewCell.swift
+++ iOSBasic_MyToDoList/iOSBasic_MyToDoList/TodoListTableViewCell.swift
@@ -1,5 +1,5 @@
 //
-//  TodoListTableViewCell.swift
+//  ToDoListTableViewCell.swift
 //  iOSBasic_MyToDoList
 //
 //  Created by t2023-m0035 on 12/13/23.
@@ -7,28 +7,28 @@
 
 import UIKit
 
-class TodoListTableViewCell: UITableViewCell {
-
-    @IBOutlet weak var todoContentsLabel: UILabel!
+class ToDoListTableViewCell: UITableViewCell {
+    @IBOutlet weak var todoTitleLabel: UILabel!
+    @IBOutlet weak var todoCompleteButton: UIButton!
+    
+    var isComplete: Bool = false {
+        didSet {
+            if isComplete == true {
+                todoCompleteButton.tintColor = UIColor.systemRed
+            } else {
+                todoCompleteButton.tintColor = UIColor.lightGray
+            }
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-    
-    
-    @IBAction func TodoCompleteButton(_ sender: UIButton) {
-        print("true(했음!) 상태면 하트를 채워줘용")
-    }
-    
-    @IBAction func TodoDeleteButton(_ sender: Any) {
-        print("X 를 누르면 To do 를 삭제해용")
-    }
-    
 }
--- iOSBasic_MyToDoList/iOSBasic_MyToDoList/ViewController.swift
+++ iOSBasic_MyToDoList/iOSBasic_MyToDoList/ViewController.swift
@@ -14,34 +14,79 @@ struct Todo {
 
 class ViewController: UIViewController {
     
-    let testData: [Todo] = [Todo(title: "밥 먹기", isComplete: false), Todo(title: "양치하기", isComplete: false), Todo(title: "왜 이렇게 졸리냐 낮잠자기(안됨)", isComplete: false),Todo(title: "강아지랑 산책가기", isComplete: false)]
+    @IBOutlet weak var TodoListTableView: UITableView!
     
-    @IBOutlet weak var ToDoListTableView: UITableView!
+    var testData: [Todo] = [Todo(title: "밥 먹기", isComplete: false), Todo(title: "양치하기", isComplete: false), Todo(title: "왜 이렇게 졸리냐 낮잠자기(안됨)", isComplete: false),Todo(title: "강아지랑 산책가기", isComplete: false)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ToDoListTableView.dataSource = self
+        // Do any additional setup after loading the view.
+        TodoListTableView.dataSource = self
     }
     
-    @IBAction func TappedAddTodoButton(_ sender: UIButton) {
-        print("To do 를 추가해주는 버튼")
+    @IBAction func AddTodoContents(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Add Todo", message: "추가할 내용을 입력해주세요.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            let inputTitle = String((alert.textFields?[0].text)!)
+            
+            self.testData.append(Todo(title: inputTitle, isComplete: false))
+            self.TodoListTableView.reloadData()
+        }))
+        alert.addAction(UIAlertAction(title: NSLocalizedString("CANCEL", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"Cancel\" alert occured.")
+        }))
+        alert.addTextField(configurationHandler: {
+            textField in
+            textField.placeholder = "To do Contents"
+        })
+        
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
 
 extension ViewController: UITableViewDataSource {
     
-    //    numberOfRowsInSection은 TableView에 표시할 행의 수를 return
+    // MARK: Cell 추가
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //    numberOfRowsInSection은 TableView에 표시할 행의 수를 return
         return testData.count
     }
     
-    //    cellForRowAt은 Cell에 표시하기 위한 Cell을 만들어서 return
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = ToDoListTableView.dequeueReusableCell(withIdentifier: "todolistCell", for: indexPath) as! TodoListTableViewCell
-        cell.todoContentsLabel.text = self.testData[indexPath.row].title
+        //    cellForRowAt은 Cell에 표시하기 위한 Cell을 만들어서 return
+        let cell = TodoListTableView.dequeueReusableCell(withIdentifier: "TodoListCell", for: indexPath) as! ToDoListTableViewCell
+        cell.selectionStyle = .none
+        cell.todoTitleLabel.text = self.testData[indexPath.row].title
+        cell.todoCompleteButton.tag = indexPath.row
+        cell.todoCompleteButton.addTarget(self, action: #selector(checkBoxButtonTapped(sender:)), for: .touchUpInside)
         return cell
     }
     
+    // MARK: Delete Table View Cell
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.testData.remove(at: indexPath.row)
+            TodoListTableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    // MARK: Complete Check
+    @objc func checkBoxButtonTapped(sender: UIButton) {
+        guard let cell = sender.superview?.superview as? ToDoListTableViewCell else {
+            return
+        }
+        
+        let index = sender.tag
+        
+        !testData[index].isComplete ? (testData[index].isComplete = true) : (testData[index].isComplete = false)
+        cell.isComplete = testData[index].isComplete
+    }
 }
+
+
