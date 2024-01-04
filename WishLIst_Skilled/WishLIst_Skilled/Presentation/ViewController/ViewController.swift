d27e439ae0a7a7ab1b5af491c5b45ada6eb61852
yenny <yennydev42@gmail.com>
2024-01-04 17:29:28 +0900

    [Add] Project Setting

---
+++ WishLIst_Skilled/WishLIst_Skilled/Presentation/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WishLIst_Skilled
+//
+//  Created by t2023-m0035 on 1/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // load test
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
