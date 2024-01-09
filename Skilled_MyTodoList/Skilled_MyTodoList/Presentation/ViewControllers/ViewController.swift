0684448b0950e331a7a8aabb2ab34965264e6018
yenny <yennydev42@gmail.com>
2024-01-09 10:14:23 +0900

    [Feat] - project settings

---
+++ Skilled_MyTodoList/Skilled_MyTodoList/Presentation/ViewControllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Skilled_MyTodoList
+//
+//  Created by t2023-m0035 on 1/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
