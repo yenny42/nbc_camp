35177e60b81af948ba4040bd715acfbde36eedfe
yenny <yennydev42@gmail.com>
2024-01-24 17:24:07 +0900

    [Feat] 심화 - 코드베이스 프로젝트 세팅 - 네비게이션 연경 - 스토리보드 삭제

---
+++ Deepening_Profile/Deepening_Profile/Presentation/View/ProfileDesignViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileDesignViewController.swift
+//  Deepening_Profile
+//
+//  Created by t2023-m0035 on 1/24/24.
+//
+
+import UIKit
+
+class ProfileDesignViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
---
+++ Deepening_Profile/Deepening_Profile/Presentation/ViewModel/MainViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewModel.swift
+//  Deepening_Profile
+//
+//  Created by t2023-m0035 on 1/24/24.
+//
+
+import Foundation
+
+class MainViewModel {
+    
+}
