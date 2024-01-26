33139de9f606cf10ef528e7dbd0466f859743a9e
yenny <yennydev42@gmail.com>
2024-01-26 15:57:03 +0900

    [Feat] 심화 - CoreData 생성,읽기 구현

--- Deepening_Profile/Deepening_Profile/Presentation/ViewModel/TodoViewModel.swift
+++ Deepening_Profile/Deepening_Profile/Presentation/ViewModel/TodoViewModel.swift
@@ -5,14 +5,90 @@
 //  Created by t2023-m0035 on 1/25/24.
 //
 
-import Foundation
+import UIKit
+import CoreData
 
 class TodoViewModel {
-    func dateFormat() -> String {
+    
+    var persistentContainer: NSPersistentContainer? {
+        (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+    }
+    
+    func saveData(title: String) {
+        guard let context = self.persistentContainer?.viewContext else { return }
+        
+        let newTodo = TodoItem(context: context)
+        
+        newTodo.id = UUID()
+        newTodo.title = title
+        newTodo.createDate = Date()
+        newTodo.isCompleted = false
+        
+        try? context.save()
+    }
+
+    func readData() -> [Todo] {
+        guard let context = self.persistentContainer?.viewContext else { return [] }
+        
+        let request = TodoItem.fetchRequest()
+        let todos = try? context.fetch(request)
+        
+        var data: [Todo] = []
+        
+        for todo in todos! {
+            let todoModel = Todo(
+                id: String(describing: todo.id),
+                title: todo.title!,
+                createDate: todo.createDate!,
+                modifyDate: todo.modifyDate ?? nil,
+                isCompleted: todo.isCompleted
+            )
+            
+            data.append(todoModel)
+        }
+        
+        return data
+    }
+    
+    func updateData() {
+        guard let context = self.persistentContainer?.viewContext else { return }
+        
+        let request = TodoItem.fetchRequest()
+        guard let todos = try? context.fetch(request) else { return }
+        
+        // update
+        let filteredData = todos.filter { $0.title == "메룽메룽" }
+        
+        for data in filteredData {
+            data.title = "water melon"
+        }
+        
+        try? context.save()
+    }
+    
+    func deleteData() {
+        guard let context = self.persistentContainer?.viewContext else { return }
+        
+        let request = TodoItem.fetchRequest()
+        guard let todos = try? context.fetch(request) else { return }
+        
+        let filteredData = todos.filter { $0.title == "water melon" }
+        
+        for data in filteredData {
+            context.delete(data)
+        }
+        
+        try? context.save()
+    }
+    
+}
+
+extension TodoViewModel {
+    func dateFormat(_ input: Date) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy.MM.dd"
         
-        let currentDate = Date()
+        let currentDate = input
         let formattedDate = dateFormatter.string(from: currentDate)
         
         return formattedDate
